c3d27e52cdf3a4e81e566d95ea56f92ae09d498b
eaglewing <r.orneving@gmail.com>
2024-10-14 14:14:27 +0200

    Front end recommender improvement. Working prototype

--- pages/recommendations.py
+++ pages/recommendations.py
@@ -12,11 +12,17 @@ def load_data(filename):
     df = pd.read_csv(filename)
     return df
 
+
 df = load_data('./data/spotify_dataset_kaggle.csv')
+df = df[["name", "artists", "release_date"]]
+df_num = load_data("./data/spotify_numerical.csv")
+df_num = df_num.set_index("name")
+
 st.title("Music recommendations")
 st.write('Find similar songs based on your preference')
 
-song_search = st.text_input("Search song by title or speaker", value="", placeholder='Enter artist or song name...')
+song_search = st.text_input("Search song by title or speaker",
+                            value="", placeholder='Enter artist or song name...')
 song_search = capitalize_words(song_search)
 
 
@@ -25,5 +31,27 @@ m2 = df["name"].str.contains(song_search)
 df_search = df[m1 | m2]
 
 if song_search:
-    st.write(df_search)
+    selected_row = st.dataframe(
+        df_search, selection_mode="single-row", on_select="rerun")
 
+    try:
+        select_index = selected_row.selection["rows"]
+
+        select_index = select_index[0]
+        row = df_search.iloc[select_index]
+    except IndexError:
+        pass
+
+try:
+    song_name = row["name"]
+    song = df_num.loc[song_name]
+    distances = np.linalg.norm(df_num-song, axis=1)
+    recommendations = 5
+    indexes = np.argpartition(distances, recommendations)
+    st.write(df.iloc[indexes[:recommendations]])
+except IndexError:
+    st.write("Select a song by checking the box to the left of the song")
+except ValueError:
+    st.write("No data")
+except NameError:
+    pass
